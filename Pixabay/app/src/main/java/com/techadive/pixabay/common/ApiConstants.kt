840e60e5b1d084b2a44b3e05690c7a5794a156a1
Roy Sylvanus <vicevanus@gmail.com>
2024-10-03 07:01:07 +0200

    Refractored api key

--- Pixabay/app/src/main/java/com/techadive/pixabay/common/ApiConstants.kt
+++ Pixabay/app/src/main/java/com/techadive/pixabay/common/ApiConstants.kt
@@ -1,8 +1,8 @@
 package com.techadive.pixabay.common
 
 object ApiConstants {
-    // replace api key field with your key here
-    const val API_KEY = "46161359-8323757e1b439da0a3aaa87d1"
+    // replace api key field with your key here 
+    const val API_KEY = ""
 
     const val BASE_URL = "https://pixabay.com/"
 }
